67c2f5cc56cd68950fbdd57299ec8f3cd0300a23
Maokang Dou <alecdou0001@gmail.com>
2022-07-05 23:42:17 +0800

    segment tree object

--- python/segment_tree.py
+++ python/segment_tree.py
@@ -2,7 +2,7 @@ class RangeSum:
     def __init__(self, nums):
         self.nums = nums
         self.N = len(self.nums)
-        self.tree = [0] * self.N * 4
+        self.tree = [0] * self.N * 4  # TODO: [think] why 4n?
         self._build(1, 0, self.N - 1)
 
     def _build(self, node: int, l: int, r: int):
@@ -87,13 +87,69 @@ class RangeMin:
         self._update(1, 0, self.N - 1, idx, val)
 
 
+class TreeNode:
+    def __init__(self, start, end, val, left=None, right=None):
+        self.start = start
+        self.end = end
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def get_mid(self):
+        return (self.start + self.end) // 2
+
+
+class SegmentTree:
+    def __init__(self, nums):
+        self.root = self.build(nums, 0, len(nums) - 1)
+
+    def build(self, nums, l, r):
+        if l == r:
+            return TreeNode(l, r, nums[l])
+        else:
+            mid = (l + r) // 2
+            left_child = self.build(nums, l, mid)
+            right_child = self.build(nums, mid + 1, r)
+            return TreeNode(l, r, left_child.val + right_child.val, left_child, right_child)
+
+    def update(self, node, idx, val):
+        if node.start == node.end:
+            node.val = val
+        else:
+            mid = node.get_mid()
+            if idx <= mid:
+                self.update(node.left, idx, val)
+            else:
+                self.update(node.right, idx, val)
+            node.val = node.left.val + node.right.val
+
+    def query(self, node, l, r):
+        if l > r:
+            return 0
+
+        if node.start == l and node.end == r:
+            return node.val
+        mid = node.get_mid()
+        return self.query(node.left, l, min(mid, r)) + self.query(node.right, max(mid + 1, l), r)
+
+
 if __name__ == '__main__':
-    rs = RangeSum([1, 2, 3, 4, 5])
-    print(rs.query(0, 4))
-    rs.update(1, -1)
-    print(rs.query(1, 4))
+    rs = RangeSum([0, 9, 5, 7, 3])
+    print(rs.query(4, 4))
+    print(rs.query(2, 4))
+    print(rs.query(1, 1))
+    rs.update(0, 3)
+    print(rs.query(0, 1))
+    rs.update(1, -3)
+    print(rs.query(0, 1))
 
     rmin = RangeMin([1, 2, -1, 4, 5])
     print(rmin.query(0, 0))
     rmin.update(4, -5)
     print(rmin.query(0, 3))
+
+    st = SegmentTree([9, -8, 7])
+    print(st.query(st.root, 0, 2))
+    st.update(st.root, 1, 1)
+    print(st.query(st.root, 0, 2))
+
