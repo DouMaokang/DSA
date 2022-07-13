d005d6868be8f97352b367e09c3cbcc7fc5658e9
Maokang Dou <alecdou0001@gmail.com>
2022-07-14 02:26:28 +0800

    segment tree practice

---
+++ python/leetcode/327.py
@@ -0,0 +1,87 @@
+class Node:
+    def __init__(self, start: int, end: int, val: int, left=None, right=None):
+        """
+        Segment tree node.
+        :param start: lower bound of interval
+        :param end: upper bound of interval
+        :param val: int
+        :param left: left child
+        :param right: right child
+        """
+        self.start = start
+        self.end = end
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def get_mid(self):
+        return (self.start + self.end) // 2
+
+
+class SegmentTree:
+    def __init__(self, n):
+        self.N = n
+        self.root = self.build(0, self.N - 1)
+
+    def build(self, l: int, r: int):
+        if l == r:
+            return Node(l, r, 0)
+        mid = (l + r) // 2
+        left_child = self.build(l, mid)
+        right_child = self.build(mid + 1, r)
+        val = left_child.val + right_child.val
+        return Node(l, r, val, left_child, right_child)
+
+    def _query(self, node: Node, left: int, right: int):
+        if left > right:
+            return 0
+
+        if node.start == left and node.end == right:
+            return node.val
+
+        mid = node.get_mid()
+        return self._query(node.left, left, min(mid, right)) + self._query(node.right, max(mid + 1, left), right)
+
+    def query(self, left: int, right: int):
+        return self._query(self.root, left, right)
+
+    def _update(self, node: Node, idx: int):
+        if node.start == node.end:
+            node.val += 1
+        else:
+            mid = node.get_mid()
+            if idx <= mid:
+                self._update(node.left, idx)
+            else:
+                self._update(node.right, idx)
+            node.val = node.left.val + node.right.val
+
+    def update(self, idx: int):
+        self._update(self.root, idx)
+
+
+def count_range_sum(self, nums: list[int], lower: int, upper: int) -> int:
+    # lower <= tree[j] - x <= upper
+    # find x where (tree[j] - upper <= x <= tree[j] - lower)
+
+    n = len(nums)
+    prefix_sum = [0] * (n + 1)
+    for i in range(n):
+        prefix_sum[i + 1] = prefix_sum[i] + nums[i]
+
+    all_nums = set()
+    for x in prefix_sum:
+        all_nums.add(x)
+        all_nums.add(x - lower)
+        all_nums.add(x - upper)
+
+    all_nums = sorted(all_nums)
+    ranks = {num: rank for rank, num in enumerate(all_nums)}
+
+    tree = SegmentTree(len(all_nums))
+    result = 0
+    for x in prefix_sum:
+        result += tree.query(ranks[x - upper], ranks[x - lower])
+        tree.update(ranks[x])
+    return result
+
---
+++ python/segment_tree/SegmentTree.md
@@ -0,0 +1,5 @@
+Practice Questions
+- LeetCode 327
+  - [My solution](../leetcode/327.py): time limit exceeded on LC
+- LeetCode 218
+  - [My solution](../leetcode/218.py): not solved yet
