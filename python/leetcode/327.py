a16020b9cec961f7714d9f68d9b41afbddaac2f1
Maokang Dou <alecdou0001@gmail.com>
2022-07-19 00:02:52 +0800

    327 dynamic segment tree

--- python/leetcode/327.py
+++ python/leetcode/327.py
@@ -85,3 +85,70 @@ def count_range_sum(self, nums: list[int], lower: int, upper: int) -> int:
         tree.update(ranks[x])
     return result
 
+
+class DynamicSegmentTree:
+    """
+    Segment tree with dynamic node insertion.
+    """
+
+    def __init__(self, lo: int, hi: int):
+        """
+        :param lo: lower bound of all possible values
+        :param hi: higher bound of allpossible values
+        """
+        self.root = Node(lo, hi, 0)
+
+    def _insert(self, node: Node, val: int):
+        if node.start == node.end:
+            node.val += 1
+        else:
+            mid = node.get_mid()
+            if not node.left:  # TODO: why do we have to place this here?
+                node.left = Node(node.start, mid, 0)
+            if not node.right:
+                node.right = Node(mid + 1, node.end, 0)
+
+            if val <= mid:
+                self._insert(node.left, val)
+            else:
+                self._insert(node.right, val)
+            if node.left:
+                node.val = node.left.val
+            if node.right:
+                node.val += node.right.val
+
+    def insert(self, val: int):
+        self._insert(self.root, val)
+
+    def _query(self, node: Node, left: int, right: int):
+        if not node or left > right:
+            return 0
+
+        if node.start == left and node.end == right:
+            return node.val
+
+        mid = node.get_mid()
+        return self._query(node.left, left, min(mid, right)) + self._query(node.right, max(mid + 1, left), right)
+
+    def query(self, left: int, right: int):
+        return self._query(self.root, left, right)
+
+
+def count_range_sum_1(self, nums: list[int], lower: int, upper: int) -> int:
+    n = len(nums)
+    prefix_sum = [0] * (n + 1)
+    for i in range(n):
+        prefix_sum[i + 1] = prefix_sum[i] + nums[i]
+
+    all_nums = set()
+    for x in prefix_sum:
+        all_nums.add(x)
+        all_nums.add(x - lower)
+        all_nums.add(x - upper)
+
+    tree = DynamicSegmentTree(min(all_nums), max(all_nums))
+    result = 0
+    for x in prefix_sum:
+        result += tree.query(x - upper, x - lower)
+        tree.insert(x)
+    return result
